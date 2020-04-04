a2900209c64a6d4a6d7737e3b24c75ef8f1f75e7
Everton Avelino <everton-vitor@hotmail.com>
2020-04-04 09:57:46 -0300

    modificando rotas e instalando nodemon

--- app/models/acoes.js
+++ app/models/acoes.js
@@ -3,5 +3,14 @@ const mongoose = require('../../db/conexao');
 const Acoes = mongoose.model('acoes',{
     nome_empresa : {type: String, required: true},
     cod_empresa : {type: Number, required: true},
+    taxa_juros : {type: Number, required: true},
+    tipo: {
+        type: String,
+        required: true,
+        message: 'Investidor deve escolher qual tipo de ação ordinaria/preferencial'
+    }
+    //tipo = ordinaria/ preferencial 
     
 });
+
+module.exports = Acoes;
